c861569660b829fc0abe4330a49e005ca749a348
dibrugh <dibrugh@gmail.com>
2023-11-30 13:11:43 +0300

    refactor: change useRef for just conditional classNames

--- cv-react/src/components/btnDarkMode/BtnDarkMode.js
+++ cv-react/src/components/btnDarkMode/BtnDarkMode.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect } from 'react'
 // кастомный хук
 import { useLocalStorage } from '../../utils/useLocalStorage';
 import detectDarkMode from '../../utils/detectDarkMode';
@@ -12,15 +12,12 @@ export default function BtnDarkMode() {
     // 'darkMode' - key, 'light' - default value. Т.е при первичной загрузке в LocalStorage попадает darkMode: "light"
     // Теперь default value определяется в зависимости от установленно темы пользователя.
     const [darkMode, setDarkMode] = useLocalStorage('darkMode', detectDarkMode());
-    const btnRef = useRef(null);
 
     useEffect(() => {
         if (darkMode === 'dark') {
             document.body.classList.add('dark');
-            btnRef.current.classList.add('dark-mode-btn_active');
         } else {
             document.body.classList.remove('dark');
-            btnRef.current.classList.remove('dark-mode-btn_active');
         }
     }, [darkMode]);
 
@@ -33,14 +30,17 @@ export default function BtnDarkMode() {
                 setDarkMode(newColorScheme);
                 }
             );
-    }, []);
+    }, [setDarkMode]);
 
     const toggleDarkMode = () => {
         setDarkMode((currentValue) => currentValue === 'light' ? 'dark' : 'light')
     }
 
+    const btnDefault = "dark-mode-btn";
+    const btnActive = "dark-mode-btn dark-mode-btn_active"
+
     return (
-        <button ref={btnRef} className="dark-mode-btn" onClick={toggleDarkMode}>
+        <button className={darkMode === 'dark' ? btnActive : btnDefault} onClick={toggleDarkMode}>
             <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
             <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
         </button>
